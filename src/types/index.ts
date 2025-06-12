0844ffc236d261b764348f3532191b1604f47b89
Angel8118 <angel8118campos@gmail.com>
2025-06-12 13:03:06 -0600

    store with zustand

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,5 @@
-import { CurrencySchema } from "../schema/cripto-schema";
 import { z } from "zod";
+import { CurrencySchema, CryptoCurrencyResponseSchema } from "../schema/cripto-schema";
 
 export type Currency = z.infer<typeof CurrencySchema>;
+export type Cryptocurrency = z.infer<typeof CryptoCurrencyResponseSchema>;
