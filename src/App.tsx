87915807f7fb97537a7a8b85cbd6b398c4aa2cf0
Angel8118 <angel8118campos@gmail.com>
2025-06-12 09:23:30 -0600

    create form

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
+import CriptoSearchForm from "./components/CriptoSearchForm";
 
 
 export default function App() {
 
   return (
     <>
+    <div className="container">
+      <h1 className="app-title">
+        Cotizador de <span>Criptomonedas</span>
+      </h1>
+      <div className="content">
+        <CriptoSearchForm/>
+      </div>
+
+    </div>
     </>
   )
 }
