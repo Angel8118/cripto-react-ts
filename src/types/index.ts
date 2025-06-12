13f4a11323430fd6504c92d95896f4c6b088f4ef
Angel8118 <angel8118campos@gmail.com>
2025-06-12 16:37:56 -0600

    calling API

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,7 @@
 import { z } from "zod";
-import { CurrencySchema, CryptoCurrencyResponseSchema } from "../schema/cripto-schema";
+import { CurrencySchema, CryptoCurrencyResponseSchema, PairSchema } from "../schema/cripto-schema";
+
 
 export type Currency = z.infer<typeof CurrencySchema>;
 export type Cryptocurrency = z.infer<typeof CryptoCurrencyResponseSchema>;
+export type Pair = z.infer<typeof PairSchema> 
